4c221aa9207a1523c6610679cca1a4f621f02b1d
puliv <pverapavez@gmail.com>
2020-11-09 20:46:56 -0300

    avanzando en home

--- src/App.js
+++ src/App.js
@@ -4,6 +4,17 @@ import './scss/App.scss';
 function App() {
   return (
     <div className="App">
+      <div className="home">
+        <div className="box-2">
+          <div className="div-top" />
+          <h1>Pulié</h1>
+          <h3>FRONT END</h3>
+          <h3>DEVELOPER</h3>
+        </div>
+        <div className="box-1">
+          <div className="div-top" />
+        </div>
+      </div>
       <Menu />
     </div>
   )
