d65f4748c897e29da436dbcc660ff8d84f496c74
puliv <pverapavez@gmail.com>
2020-11-16 14:42:24 -0300

    arreglando estilos

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -16,23 +16,39 @@ function Contact() {
       <div className="div-main">
         <div className="info">
           <h2>Contacto</h2>
-          <span>Si tienes una idea, no dudes en contactarme, responderé lo antes posible, gracias por tu interés.</span>
+          <span>Si tienes una idea y quieres trabajar conmigo, no dudes en contactarme, responderé lo antes posible, gracias por tu interés.</span>
           <ul>
             <li>
-              <Gmail color="#000" size={30} />
-              <span>Gmail</span>
+              <Gmail color="#000" size={40} />
+              <span>
+                <a href="mailto:pverapavez@gmail.com">
+                  Gmail
+                </a>
+              </span>
             </li>
             <li>
-              <Linkedin color="#000" size={30} />
-              <span>LinkedIn</span>
+              <Linkedin color="#000" size={40} />
+              <span>
+                <a href="https://www.linkedin.com/in/pulie/" target="_blank" rel="noopener noreferrer">
+                  LinkedIn
+                </a>
+              </span>
             </li>
             <li>
-              <Github color="#000" size={30} />
-              <span>GitHub</span>
+              <Github color="#000" size={40} />
+              <span>
+                <a href="https://github.com/puliv" target="_blank" rel="noopener noreferrer">
+                  GitHub
+                </a>
+              </span>
             </li>
             <li>
-              <Behance color="#000" size={30} />
-              <span>BeHance</span>
+              <Behance color="#000" size={40} />
+              <span>
+                <a href="https://www.behance.net/pulie" target="_blank" rel="noopener noreferrer">
+                  Behance
+                </a>
+              </span>
             </li>
           </ul>
         </div>
